29003e691238b367dee091f0b492819f5e7c667d
aka <aaronkaltman@gmail.com>
2023-12-17 21:45:48 -0500

    lib constants

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
+import { MAX_CHARS } from "../lib/constants";
 
 export default function FeedbackForm() {
   const [text, setText] = useState("");
+  const charCount = text.length;
+  const charLeft = MAX_CHARS - charCount;
 
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="abc" />
+      <textarea
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        value={text}
+        id="feedback-textarea"
+        placeholder="abc"
+      />
       <label htmlFor="feedback-textarea">
         Enter Your Corporate Feedback Here. Remember to # the company name.
       </label>
       <div>
-        <p className="u-italic">150</p>
-        <button>
+        <p className="u-italic">{charLeft}</p>
+        <button
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log("submit");
+          }}
+        >
           <span>Submit</span>
         </button>
       </div>
